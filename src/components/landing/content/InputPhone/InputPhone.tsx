ee9f32a03da39860f137ae66621c46baa0658d39
deniskeller <dentean@mail.ru>
2023-11-19 13:26:21 +0300

    Registration steps

---
+++ src/components/landing/content/InputPhone/InputPhone.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
+import s from './InputPhone.module.scss';
+
+interface Props {
+  placeholder?: string;
+  className?: string;
+  error?: string | boolean;
+  value: string;
+  disabled?: boolean;
+  onChange(value: string | number): void;
+}
+
+const InputPhone: React.FC<Props> = ({
+  value,
+  error,
+  placeholder,
+  className = '',
+  onChange,
+  disabled = false,
+}) => {
+  return (
+    <div
+      className={`${s.BaseInput} ${className} ${
+        disabled ? s.BaseInput_Disabled : ''
+      }`}
+    >
+      <PhoneInput
+        placeholder={placeholder}
+        country={'ru'}
+        value={value}
+        onChange={onChange}
+        enableSearch
+        searchPlaceholder="Search"
+        searchNotFound="Nothing found"
+        inputClass={error ? 'phone-input-error' : ''}
+        disabled={disabled}
+      />
+    </div>
+  );
+};
+export default InputPhone;
