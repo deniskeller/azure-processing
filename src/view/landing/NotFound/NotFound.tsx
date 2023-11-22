6f400964999c87e033f1f5c9a7aca4c68674bb4d
deniskeller <dentean@mail.ru>
2023-11-22 16:17:37 +0300

    change images

--- src/view/landing/NotFound/NotFound.tsx
+++ src/view/landing/NotFound/NotFound.tsx
@@ -2,7 +2,6 @@ import { BaseButton } from '@base/index';
 import { useRouter } from 'next/router';
 import React from 'react';
 import s from './NotFound.module.scss';
-import Image from 'next/image';
 
 const NotFound: React.FC = () => {
   const router = useRouter();
@@ -622,17 +621,6 @@ const NotFound: React.FC = () => {
           </clipPath>
         </defs>
       </svg>
-
-      {/* <div className={s.NotFound_Image}>
-        <Image
-          src="/pictures/images/404.png"
-          width={500}
-          height={500}
-          priority
-          quality={100}
-          alt="404 image"
-        />
-      </div> */}
 
       <BaseButton
         className={s.NotFound_Button}
