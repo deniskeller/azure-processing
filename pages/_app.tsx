f1f0e2342561cc2fda9b3f0d96b4bf39ad6e3618
deniskeller <dentean@mail.ru>
2023-12-08 15:11:46 +0400

    ref meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,7 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
           content="Azure Processing is professional Agent with many years of experience made to solicit merchants for an acquiring banks or payment service providers under their name."
         />
         {/* <meta property="og:type" content="website" /> */}
-        <meta property="og:url" content="Azure processing" />
+        {/* <meta property="og:url" content="Azure processing" /> */}
         <meta
           property="og:title"
           content="Azure processing. Your best acquiring provider"
